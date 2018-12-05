dc57389831a6a0380a739a47f37025af65c34c8e
ericstotle <43819683+ericstotle@users.noreply.github.com>
2018-12-05 12:12:39 +0100

    Update HapaxLegomena.hs
    Fairly primitive. Takes a text file and uses set membership relations (elem and notElem) to make a list of the hapax legomena, the words that only occur once.

--- HapaxLegomena.hs
+++ HapaxLegomena.hs
@@ -1,5 +1,3 @@
--- Fairly primitive. Takes a text file and produces a list of the hapax legomena, the words that only occur once.
-
 import Data.Char
 import Data.List
 import System.Environment
@@ -10,15 +8,13 @@ import qualified Data.Set as Set
 
 main = do
   putStrLn "Here are the hapax legomena"
-  contents <- readFile "texst.txt"
+  contents <- readFile "yourfilegoeshere.txt"
   let wordlist  = map toLower contents
       wordlist1 = map noPunc $ words wordlist
       wordlist2 = sort $ unique wordlist1
   print wordlist2
 
-  --get rid of common words make corewordList
-
--- convert contents into a list of words. get relative frequency and make tuples
+-- convert contents into a list of words. get relative frequency list and and zip them to make tuples
 
 percent :: Int -> Int -> Float
 percent n m = (fromIntegral n / fromIntegral m) * 100
@@ -28,13 +24,13 @@ frequency words' = [percent (occs word) totalwords | word <- words']
         where occs word = length (filter (==word) words')
               totalwords = length words'
 
---counts number of times p occurs in [a]
+-- count number of times p occurs in [a]. (recursion + pattern matching with boolean guards)
 count :: Eq a => a -> [a] -> Int
 count p []                 = 0
 count p (x:xs) | p == x    = 1 + count p xs
                | otherwise = count p xs
 
-
+-- 
 unique :: (Eq a) => [a] -> [a]
 unique [] = []
 unique (x:xs)
